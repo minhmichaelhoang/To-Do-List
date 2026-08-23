5c3c4d3c05ef3ffd8fb20e94a5ba2658575e535c
Michael Hoang <michael.hoang@gmx.de>
2026-08-24 01:22:24 +0200

    added test coverage and test for frontend

---
+++ frontend/src/components/ui/Checkbox.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/Button.tsx"
+
+interface Checkbox {
+	onBoxChecked: () => void;
+}
+
+export function Checkbox(taskId: string) {
+	return (
+		<Button onClick={() => }>
+
+		</Button>
+	)
+}
